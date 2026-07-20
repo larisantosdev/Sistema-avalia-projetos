8dad40ee60e8a7796e4e483e2b2cdd1f87b8e9f3
Larissa Santos <laridsantoslima@gmail.com>
2026-07-19 21:20:56 -0300

    adiciona o arquivo program.cs ao projeto

---
+++ program.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Globalization;
+
+class Projeto{
+  
+  public string Nome {get; set;}
+  public string Consultor {get; set;}
+  public int Qualidade {get; set;}
+  public int Prazo {get; set;}
+  public int Satisfacao {get; set;}
+
+  // metodo que calculo a media das notas
+  
+  public double CalcularMedia(){
+    
+    return (Satisfacao + Qualidade + Prazo) / 3.0;
+    
+  }
+  
+
+// TODO: Crie a Classe que representa um Projeto com suas informações
+
+     // Nome do projeto
+    // Nome do consultor
+    // Nota de qualidade
+    // Nota de prazo
+    // Nota de satisfação
+
+    // TODO: Crie o método que calcula a média das notas
+   
+
+    // Método que imprime as informações no formato solicitado
+    
+    
+    
+    
+    
+    public void ExibirInformacoes()
+    {
+        Console.WriteLine($"Projeto: {Nome}");
+        Console.WriteLine($"Consultor: {Consultor}");
+        Console.WriteLine($"Notas: Qualidade: {Qualidade}, Prazo: {Prazo}, Satisfacao: {Satisfacao}");
+        Console.WriteLine($"Media: {CalcularMedia():F2}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        string linhaProjeto = Console.ReadLine(); 
+        string nomeProjeto = linhaProjeto.Substring(9); 
+
+        string linhaConsultor = Console.ReadLine(); 
+        string nomeConsultor = linhaConsultor.Substring(11); 
+        
+        string linhaNotas = Console.ReadLine(); 
+        string notasApenas = linhaNotas.Substring(7); 
+        string[] partes = notasApenas.Split(", "); 
+
+        // Criação do objeto Projeto com os dados extraídos
+        Projeto projeto = new Projeto
+        {
+            Nome = nomeProjeto,
+            Consultor = nomeConsultor,
+            Qualidade = int.Parse(partes[0]),
+            Prazo = int.Parse(partes[1]),
+            Satisfacao = int.Parse(partes[2])
+        };
+
+        projeto.ExibirInformacoes();
+    }
+}
